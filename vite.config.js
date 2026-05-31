2abf936d8e08da4b268189bba2fb850f7edce376
TerezaN <tete.leticia@gmail.com>
2026-05-30 22:53:19 -0300

    Configure GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://github.com/Trooper2123/minha_agenda/',
   test: {
     environment: 'jsdom',
     globals: true,
